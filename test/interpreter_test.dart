b740fd33982f673121e9090b0078964bb9cdf5fb
Moustapha Kodjo Amadou <107993382+kodjodevf@users.noreply.github.com>
2025-06-20 10:35:15 +0100

    rm

--- test/interpreter_test.dart
+++ test/interpreter_test.dart
@@ -7,7 +7,7 @@ Matcher throwsRuntimeError(dynamic messageMatcher) {
 }
 
 dynamic execute(String source, {Object? args}) {
-  final d4rt = D4rt()..setDebug(true);
+  final d4rt = D4rt()..setDebug(false);
   return d4rt.execute(
       library: 'package:test/main.dart',
       args: args,
